103d93de978df219fdc16cb18468f68ab4d2b92c
Aabidh musthaq <113989368+Aabidh1@users.noreply.github.com>
2023-11-04 12:17:34 +0530

    Add files via upload
    this is first step of creating library management system

---
+++ Group assaigmne t/mysite/lims_app/views.py
@@ -0,0 +1,15 @@
+from django.shortcuts import render
+from django.contrib import admin
+from django.http import HttpResponse
+from django.shortcuts import render, redirect
+
+# Create your views here.
+def home(request):
+    return render(request, "index.html", context={})
+
+def shopping(request):
+    return HttpResponse("welcome to shopping")
+
+def save_student(request):
+    Student_name = request.POST['Student_name']
+    return render(request, "Welcome.html", context={'Student_name': Student_name})
