2701000d5ab6981a43ce6011bdbcf17332fee1e1
VidushiXL <vidushiur2000@gmail.com>
2023-11-20 15:30:36 +0530

    untracked Files

--- Group assaigmne t/mysite/lims_app/models.py
+++ Group assaigmne t/mysite/lims_app/models.py
@@ -17,7 +17,7 @@ from django.db import models
 
 class books(models.Model):
     book_name = models.CharField(max_length=200)
-    isbn = models.CharField(max_length=13, unique=True)
+    ISBN = models.CharField(max_length=13, unique=True)
     author = models.CharField(max_length=100)
     category = models.CharField(max_length=50)
 
