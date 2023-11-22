3f8fb15186bfc59900f9bfc179425ed536c5b623
vishalgimhan <vishalgimhan@gmail.com>
2023-11-22 10:58:29 +0530

    changes

--- Group assaigmne t/mysite/lims_app/views.py
+++ Group assaigmne t/mysite/lims_app/views.py
@@ -6,7 +6,7 @@ from django.shortcuts import render, redirect
 
 from django.http import JsonResponse
 from django.shortcuts import get_object_or_404
-
+ 
 # Create your views here.
 from .models import *
 
